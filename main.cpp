85356c9c1c3284a920aafaf8f7b8de245fd91f05
bnjam <benjamin.smith122490@gmail.com>
2017-09-15 21:21:38 -0700

    working implementation with class example

--- main.cpp
+++ main.cpp
@@ -21,17 +21,25 @@
 int main() {
 	
 	// Store bit string
-	std::string ham; 
+	std::string input; 
+	int ham[7];
 
 	// Get input from user
-	std::cout << "Enter 8 bit string:" << std::endl;
-	std::cin >> ham;
-	// Convert to char array
-	ham = ham.c_str();
+	std::cout << "Enter 7 bit string:" << std::endl;
+	std::cin >> input;
+	
+	// Convert user input from string to int array 
+	input = input.c_str();
+	for(int i=0; i<7; i++) {
+		ham[i] = (int)input[i] - '0';
+	}
 
-	std::cout << ham[4] << std::endl; // Debug print statement
+
+	//std::cout << ham[0] << std::endl; // Debug print statement
 
 	/**
+	 * Program conceptual layout
+	 * 
 	 * | P1 | P2 | D1 | P3 | D2 | D3 | D4 | P4 |
 	 * | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  |
 	 *
@@ -46,12 +54,15 @@ int main() {
 	 * 1 0 0 0 | P4  | 8
 	 */
 
+	// Set parity bits
 	int p1, p2, p3, p4;
 	p1 = (ham[3-1] ^ ham[5-1] ^ ham[7-1]);
 	p2 = (ham[3-1] ^ ham[6-1] ^ ham[7-1]);
 	p3 = (ham[5-1] ^ ham[6-1] ^ ham[7-1]);
-	p4 = (ham[0] ^ ham[1] ^ ham[2] ^ ham[3] ^ ham[4] ^ ham[5] ^ ham[6] ^ ham[7]);
+	p4 = (ham[1-1] ^ ham[2-1] ^ ham[3-1] ^ ham[4-1] ^ ham[5-1] ^ ham[6-1] ^ ham[7-1]);
 
+	// Check if parity bits are the same in the given hamming code
+	// Return which bits are in error
 	if(p1 != ham[1-1] && p2 == ham[2-1] && p3 == ham[4-1]) {
 		std::cout << "P1 is in error" << std::endl;
 	} else if(p1 == ham[1-1] && p2 != ham[2-1] && p3 != ham[4-1]) {
@@ -69,27 +80,43 @@ int main() {
 	}
 
 
+	// Set check bits
 	int c1, c2, c3, c4;
 	c1 = (ham[1-1] ^ ham[3-1] ^ ham[5-1] ^ ham[7-1]);
 	c2 = (ham[2-1] ^ ham[3-1] ^ ham[6-1] ^ ham[7-1]);
 	c3 = (ham[4-1] ^ ham[5-1] ^ ham[6-1] ^ ham[7-1]);
 
-	c4 = (ham[0]^ham[1]^ham[2]^ham[3]^ham[4]^ham[5]^ham[6]^ham[7]);
+	// Set 4th check bit
+	int tmp = 0;
+	for(int i=0; i<7; i++) {
+		tmp = tmp^ham[i];
+	}
+	//c4 = (ham[0]^ham[1]^ham[2]^ham[3]^ham[4]^ham[5]^ham[6]); // Long implementation
+	c4 = tmp;
 
-	std::cout << std::to_string(p3) << std::endl;
+	// Print 4th check bit status
+	std::cout << "c4 is " << std::to_string(c4) << std::endl;
 
+	// put check bits into array for converting to string
 	int check[3];
 	check[0] = c3;
 	check[1] = c2;
 	check[2] = c1; 
 
+	// Convert check bits into string to print for user
 	std::string error;
 	for(int i=0; i<3; i++) {
 		error += std::to_string(check[i]);
 	}
 
-	int syndrome = (c1 ^ c2 ^ c3);
+	// Determine syndrome
+	int syndrome = p4; //(c1 ^ c2 ^ c3);
 
+	// Print syndrome for user
+	std::cout << "syndrome is: " << syndrome << std::endl;
+
+	// Check syndrome against 4th check bit to determine 
+	// error status in given hamming code
 	if(c4 == 0 && syndrome == 0) {
 		std::cout << "No error" << std::endl;
 	} else if(c4 == 0 && syndrome != 0) {
@@ -100,17 +127,27 @@ int main() {
 		std::cout << "1 bit is in error.. correcting..." << std::endl << std::endl;
 		std::cout << "The " << error << " bit is in error" << std::endl << std::endl;
 
+		// Print check bit result for user
+		std::cout << "Error at: " << error << std::endl;
+		// Fix given hammign code
 		int fix = pow(check[0],2) + pow(check[1],2) + pow(check[2],2);
-		if(ham[fix] == '0') {
-			ham[fix] = '1';
-		} else if(ham[fix] == '1') {
-			ham[fix] = '0';
+		if(ham[fix] == 0) {
+			ham[fix] = 1;
+		} else if(ham[fix] == 1) {
+			ham[fix] = 0;
 		} else {
 			std::cout << "uh oh" << std::endl;
 		}
 
-		std::cout << "Fixed code is " << ham << std::endl;
+		// Convert int array into string for printing for user
+		std::string output;
+		for(int i=0; i<7; i++) {
+			output += std::to_string(ham[i]);
+		}
 
+		// Print fixed hamming code string for user
+		std::cout << "Fixed code is " << output << std::endl;
+		
 
 	}
 
